f151d37ee1bf9e19c5986e3da0d8bb1dddc7fced
Sumeet Batra <sbatra@nvidia.com>
2024-09-24 19:31:06 -0700

    svea slurm long runner

--- slurm/train_svea.sh
+++ slurm/train_svea.sh
@@ -8,14 +8,17 @@
 
 export MUJOCO_GL=egl
 
-SEED=0
+set -- 0 1 2 3 4
 DOMAIN="cartpole"
 TASK="balance"
-RUN_NAME="svea_${DOMAIN}_${TASK}_seed_${SEED}"
-srun python -m src.train --algorithm=svea \
-                         --wandb_group=svea_${DOMAIN}_${TASK}_baseline \
-                         --wandb_name=$RUN_NAME \
-                         --domain_name=$DOMAIN \
-                         --task_name=$TASK \
-                         --train_steps=1000k \
-                         --seed=$SEED
+for seed in "$@";
+  do echo "Running seed $seed"
+  RUN_NAME="svea_${DOMAIN}_${TASK}_seed_${seed}"
+  srun python -m src.train --algorithm=svea \
+                           --wandb_group=svea_${DOMAIN}_${TASK}_baseline \
+                           --wandb_name=$RUN_NAME \
+                           --domain_name=$DOMAIN \
+                           --task_name=$TASK \
+                           --train_steps=500k \
+                           --seed="$seed"
+done
